48baf11d057e3949839c6370512929f53effa079
moricho <ikeda.morito@gmail.com>
2020-02-13 14:09:34 +0900

    refactor

--- dockeeeern.go
+++ dockeeeern.go
@@ -11,10 +11,19 @@ import (
 )
 
 func Run() {
+	clone()
+}
+
+// clone: プロセスのcloneをし,その子プロセスにおいて各Namespaceの分離、UID/GIDのマッピング、必要なファイルシステムのマウントなどを行う
+func clone() {
 	cmd := exec.Command("/bin/sh")
 	cmd.SysProcAttr = &unix.SysProcAttr{
-		Cloneflags: unix.CLONE_NEWIPC | unix.CLONE_NEWNET | unix.CLONE_NEWNS |
-			unix.CLONE_NEWPID | unix.CLONE_NEWUSER | unix.CLONE_NEWUTS,
+		Cloneflags: unix.CLONE_NEWIPC | 
+			unix.CLONE_NEWNET | 
+			unix.CLONE_NEWNS |
+			unix.CLONE_NEWPID | 
+			unix.CLONE_NEWUSER | 
+			unix.CLONE_NEWUTS,
 		UidMappings: []syscall.SysProcIDMap{
 			{
 				ContainerID: 0,
